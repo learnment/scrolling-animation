c39ce60220d04bb90bc9eb1df38682b6345663e8
John Nguyen <john.nguyen09@outlook.com>
2021-11-16 15:40:47 +1100

    Set up GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+const withOptimizedImages = require('next-optimized-images');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withOptimizedImages({
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? '/scrolling-animation' : '',
+});
