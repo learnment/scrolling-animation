da3bb95524c0f4b244893843412847c5c0066e05
john-nguyen09 <john-nguyen09@users.noreply.github.com>
2021-11-22 06:33:29 +0000

    deploy: e027fcd9c142672bbdf48ed60773b0b9d24ae253

---
+++ _next/static/wEBg_1vQJXgQpTXzMoKuN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bf725dd8c24e695e.css","static/chunks/pages/index-23b7e6af77503a52.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
